fcc41daedb2a627628857bceefeeeadfaef6117f
Alex Kolpakov <akolpakov@tuble.pw>
2024-05-21 20:54:38 +0200

    #5 catalog client tests

--- internal/catalogClient_mock.go
+++ internal/catalogClient_mock.go
@@ -1,8 +1,11 @@
 package internal
 
 import (
+	"context"
+
 	pb "github.com/akolpakov-somehash/headless-ecom-protos/gen/go/catalog"
 	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc"
 )
 
 // MockCatalogClient is a mock implementation of the CatalogClient
@@ -31,4 +34,34 @@ func (m *MockCatalogClient) GetProductInfo(id uint64) (*pb.Product, error) {
 		return args.Get(0).(*pb.Product), args.Error(1)
 	}
 	return nil, args.Error(1)
+}
+
+// Mock implementation of the ProductInfoClient
+type MockProductInfoClient struct {
+	mock.Mock
+}
+
+func (m *MockProductInfoClient) GetProductList(ctx context.Context, in *pb.Empty, opts ...grpc.CallOption) (*pb.ProductList, error) {
+	args := m.Called(ctx, in)
+	return args.Get(0).(*pb.ProductList), args.Error(1)
+}
+
+func (m *MockProductInfoClient) GetProductInfo(ctx context.Context, in *pb.ProductId, opts ...grpc.CallOption) (*pb.Product, error) {
+	args := m.Called(ctx, in)
+	return args.Get(0).(*pb.Product), args.Error(1)
+}
+
+func (m *MockProductInfoClient) AddProduct(ctx context.Context, in *pb.Product, opts ...grpc.CallOption) (*pb.ProductId, error) {
+	args := m.Called(ctx, in)
+	return args.Get(0).(*pb.ProductId), args.Error(1)
+}
+
+func (m *MockProductInfoClient) UpdateProduct(ctx context.Context, in *pb.Product, opts ...grpc.CallOption) (*pb.Empty, error) {
+	args := m.Called(ctx, in)
+	return args.Get(0).(*pb.Empty), args.Error(1)
+}
+
+func (m *MockProductInfoClient) DeleteProduct(ctx context.Context, in *pb.ProductId, opts ...grpc.CallOption) (*pb.Empty, error) {
+	args := m.Called(ctx, in)
+	return args.Get(0).(*pb.Empty), args.Error(1)
 }
